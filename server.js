eee18544d455e0eb25bb7324bb8b125f9cef0c2e
alifwahyu112 <152054204+alifwahyu112@users.noreply.github.com>
2026-04-30 19:14:36 +0700

    update backend navigasi

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ require('dotenv').config();
 const app = express();
 const saltRounds = 10; 
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
 // ===== Middleware =====
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
